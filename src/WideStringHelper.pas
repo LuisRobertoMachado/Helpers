f7eccca780fcf4b19a6a88afca4a747bff3ffe97
Automafour Soluções em Sistemas e Informática LTDA <60482676+LuisRobertoMachado@users.noreply.github.com>
2024-09-25 17:46:21 -0300

    Criada função IsEmpty para widestring

--- src/WideStringHelper.pas
+++ src/WideStringHelper.pas
@@ -4,10 +4,12 @@ interface
 
 type
   TWideStringHelper = record helper for WideString
+  private
+    const Empty: widestring = '';
   public
     procedure Concatenar(const ASubString: WideString; const Separator: WideString = '');
+    function IsEmpty: Boolean;
   end;
-
 implementation
 
 procedure TWideStringHelper.Concatenar(const ASubString: WideString; const Separator: WideString = '');
@@ -16,6 +18,11 @@ begin
     Self := ASubString
   else
     Self := Self + Separator + ASubString;
+end;
+
+function TWideStringHelper.IsEmpty: Boolean;
+begin
+  Result := Self = Empty;
 end;
 
 end.
